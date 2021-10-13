db0447f6192dd7932e21bb9e8a1fe878cac1284f
wdiogu <88385256+wdiogu@users.noreply.github.com>
2021-10-13 10:28:41 -0400

    Added AssignBoardingPenalty (#35)
    * Added AssignBoardingPenalty
    
    * updated Assign Boarding Penalty to read scenario numbers as an array of int
    
    * Updates to Assign Boarding Penalty based on PR#35 suggested changes
    
    * Updated method of handling input from xtmf in Assign Boarding Penalty
    
    * Removed all dead codes from Assign Boarding Penalty
    
    * Removal of dead codes from AssignBoardingPenalty

---
+++ TMG.EMME/TMG.EMME.Test/Assign/AssignBoardingPenaltyTest.cs
@@ -0,0 +1,83 @@
+﻿/*
+    Copyright 2021 University of Toronto
+
+    This file is part of TMG.EMME for XTMF2.
+
+    TMG.EMME for XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TMG.EMME for XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TMG.EMME for XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using XTMF2;
+
+
+namespace TMG.Emme.Test.Assign
+{
+    [TestClass]
+    public class AssignBoardingPenaltyTest : TestBase
+    {
+        [TestMethod]
+        public void AssignBoardingPenalty()
+        {
+            Assert.IsTrue(
+                Helper.Modeller.Run(null, "tmg2.Assign.assign_boarding_penalty",
+                JSONParameterBuilder.BuildParameters(writer =>
+                {
+                    writer.WritePropertyName("scenario_numbers");
+                    writer.WriteStartArray();
+                    writer.WriteNumberValue(1);
+                    writer.WriteNumberValue(2);
+                    writer.WriteEndArray();
+                    writer.WritePropertyName("penalty_filter_string");
+                    writer.WriteStartArray();
+                    writer.WriteStartObject();
+                    writer.WriteString("label", "GO Train");
+                    writer.WriteString("filter", "mode=r");
+                    writer.WriteNumber("initial", 1.0);
+                    writer.WriteNumber("transfer", 1.0);
+                    writer.WriteNumber("ivttPerception", 1.0);
+                    writer.WriteEndObject();
+                    writer.WriteEndArray();
+
+
+                }), LogbookLevel.Standard));
+        }
+        [TestMethod]
+        public void AssignBoardingPenaltyModule()
+        {
+            var filterString = new[]
+            {
+                new Emme.Assign.AssignBoardingPenalty.PenaltyFilter()
+                {
+                    Label = Helper.CreateParameter("GO Train"),
+                    Filter = Helper.CreateParameter("mode=r"),
+                    Initial = Helper.CreateParameter(1.0f),
+                    Transfer = Helper.CreateParameter(1.0f),
+                    IvttPerception = Helper.CreateParameter(1.0f)
+                }
+            };
+
+            var module = new Emme.Assign.AssignBoardingPenalty()
+            {
+                Name = "AssignBoardingPenalty",
+                ScenarioNumbers = Helper.CreateParameter(new int[] { 1, 2 }),
+                PenaltyFilterString = Helper.CreateParameter(filterString),
+        };
+            module.Invoke(Helper.Modeller);
+        }
+    }
+}
---
+++ TMG.EMME/TMG.EMME/Assign/AssignBoardingPenalty.cs
@@ -0,0 +1,99 @@
+﻿/*
+    Copyright 2021 University of Toronto
+
+    This file is part of TMG.EMME for XTMF2.
+
+    TMG.EMME for XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TMG.EMME for XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TMG.EMME for XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using XTMF2;
+
+namespace TMG.Emme.Assign
+{
+    [Module(Name = "Assign Boarding Penalty", Description = "Assigns line-specific boarding penalties",
+        DocumentationLink = "http://tmg.utoronto.ca/doc/2.0")]
+    public class AssignBoardingPenalty : BaseAction<ModellerController>
+    {
+        [Parameter(Name = "Scenario Number", Description = "The scenario number to assign boarding penalty to.",
+            Index = 0)]
+        public IFunction<int[]> ScenarioNumbers;
+
+        [Parameter(Name = "Penalty Filter String", Description = "A colon seperated list of penalty in the order label:filter:initial:transfer:ivttPerception",
+            Index = 1)]
+        public IFunction<PenaltyFilter[]> PenaltyFilterString;
+
+        public override void Invoke(ModellerController context)
+        {
+            context.Run(this, "tmg2.Assign.assign_boarding_penalty", JSONParameterBuilder.BuildParameters(writer =>
+            {
+                writer.WritePropertyName("scenario_numbers");
+                writer.WriteStartArray();
+                foreach (var scenario in ScenarioNumbers.Invoke())
+                {
+                    writer.WriteNumberValue(scenario);
+                }
+                writer.WriteEndArray();
+                
+                writer.WritePropertyName("penalty_filter_string");
+                writer.WriteStartArray();
+                foreach (var penaltyFilter in PenaltyFilterString.Invoke())
+                {
+                    writer.WriteStartObject();
+                    writer.WriteString("label", penaltyFilter.Label.Invoke());
+                    writer.WriteString("filter", penaltyFilter.Filter.Invoke());
+                    writer.WriteNumber("initial", penaltyFilter.Initial.Invoke());
+                    writer.WriteNumber("transfer", penaltyFilter.Transfer.Invoke());
+                    writer.WriteNumber("ivttPerception", penaltyFilter.IvttPerception.Invoke());
+                    writer.WriteEndObject();
+                }
+                writer.WriteEndArray();
+
+            }), LogbookLevel.Standard);
+
+        }
+        public class PenaltyFilter : BaseFunction<PenaltyFilter>
+        {
+            [Parameter(Name = "label", Description = "The line group name e.g. GO Train.",
+            Index = 0)]
+            public IFunction<string> Label;
+
+            [Parameter(Name = "filter", Description = "The network selector expression",
+                Index = 1)]
+            public IFunction<string> Filter;
+
+            [Parameter(Name = "initial", Description = "The number representing the initial boarding penalty",
+                Index = 2)]
+            public IFunction<float> Initial;
+
+            [Parameter(Name = "transfer", Description = "The number representing the transfer boarding penalty",
+                Index = 3)]
+            public IFunction<float> Transfer;
+
+            [Parameter(Name = "ivttPerception", Description = "The number representing the IVTT perception Factor",
+                Index = 4)]
+            public IFunction<float> IvttPerception;
+
+            public override PenaltyFilter Invoke()
+            {
+                return this;
+            }
+        }
+    }
+
+
+}
