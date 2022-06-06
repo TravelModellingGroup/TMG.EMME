125ba2e56f5e5aa034ec5afab268ad82307b4175
wdiogu <88385256+wdiogu@users.noreply.github.com>
2022-06-06 15:55:31 -0400

    added FBTN to scenario title (#76)

--- TMG.EMME/TMGToolbox2/src/Generate/generate_hypernetwork_from_schema.py
+++ TMG.EMME/TMGToolbox2/src/Generate/generate_hypernetwork_from_schema.py
@@ -288,7 +288,7 @@ class GenerateHypernetworkFromSchema(_m.Tool()):
                     new_scenario = _bank.copy_scenario(
                         base_scenario.id, parameters["new_scenario"], copy_path_files=False, copy_strat_files=False
                     )
-                    new_scenario.title = parameters["new_scenario_title"]
+                    new_scenario.title = parameters["new_scenario_title"] + " - FBTN"
                     new_scenario.publish_network(network, resolve_attributes=True)
                     _MODELLER.desktop.refresh_needed(True)
 
