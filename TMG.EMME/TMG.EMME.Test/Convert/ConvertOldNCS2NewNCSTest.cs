4260b609ec7e09c9dff24351aa705c221f7bcfc2
wdiogu <88385256+wdiogu@users.noreply.github.com>
2022-06-13 17:14:49 -0400

    Ncs conversion initial commit (#78)
    * added Convert Old NCS to New NCS
    
    * added Convert Old NCS to New NCS test
    
    * added Convert Old NCS to New NCS initial commit
    
    * fixed name conflict
    
    * deleted additional lines

---
+++ TMG.EMME/TMG.EMME.Test/Convert/ConvertOldNCS2NewNCSTest.cs
@@ -0,0 +1,63 @@
+/*
+    Copyright 2022 University of Toronto
+
+    This file is part of TMG.EMME for XTMF2.
+
+    TMG.EMME for XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TMG.EMME for XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TMG.EMME for XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using XTMF2;
+
+namespace TMG.Emme.Test.Convert
+{
+    [TestClass]
+    public class ConvertOldNCS2NewNCSTest : TestBase
+    {
+        [TestMethod]
+        public void ConvertOldNCS2NewNCS()
+        {
+            const int scenarioNumber = 1;
+            Helper.ImportNetwork(scenarioNumber, "TestFiles/base_network.nwp");
+            Assert.IsTrue(
+                Helper.Modeller.Run(null, "tmg2.Convert.convert_old_ncs_to_new_ncs",
+                JSONParameterBuilder.BuildParameters(writer =>
+                {
+                    writer.WriteNumber("old_ncs_scenario", scenarioNumber);
+                    writer.WriteNumber("new_ncs_scenario", 2);
+                    writer.WriteString("station_centroid_file", "TestFiles/zone_centriods.csv");
+                    writer.WriteString("zone_centroid_file", "TestFiles/zone_centriods_map.csv");
+                }), LogbookLevel.Standard));
+        }
+        [TestMethod]
+        public void ConvertOldNCS2NewNCSModule()
+        {
+            const int scenarioNumber = 1;
+            Helper.ImportNetwork(scenarioNumber, "TestFiles/base_network.nwp");
+            var module = new Emme.Convert.ConvertOldNCS2NewNCS()
+            {
+                OldScenarioNumber = Helper.CreateParameter(scenarioNumber),
+                NewScenarioNumber = Helper.CreateParameter(2),
+                StationCentroidFile = Helper.CreateParameter(Path.GetFullPath("TestFiles/zone_centriods_map.csv")),
+                ZoneCentroidFile = Helper.CreateParameter(Path.GetFullPath("TestFiles/zone_centriods.csv")),
+            };
+            module.Invoke(Helper.Modeller);
+        }
+    }
+}
+
---
+++ TMG.EMME/TMG.EMME/Convert/ConvertOldNCS2NewNCS.cs
@@ -0,0 +1,57 @@
+/*
+    Copyright 2022 University of Toronto
+
+    This file is part of TMG.EMME for XTMF2.
+
+    TMG.EMME for XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TMG.EMME for XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TMG.EMME for XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TMG.Emme;
+using XTMF2;
+
+namespace TMG.Emme.Convert
+{
+    [Module(Name = "Convert OldNCS 2 NewNCS Standards", Description = "Converts the old NCS standards to the most recent.",
+        DocumentationLink = "http://tmg.utoronto.ca/doc/2.0")]
+    public class ConvertOldNCS2NewNCS : BaseAction<ModellerController>
+    {
+        [Parameter(Name = "Old NCS Scenario", Description = "",
+            Index = 0)]
+        public IFunction<int> OldScenarioNumber;
+        [Parameter(Name = "New NCS Scenario", Description = "",
+            Index = 1)]
+        public IFunction<int> NewScenarioNumber;
+        [Parameter(Name = "Station Centroid File", Description = "",
+            Index = 2)]
+        public IFunction<string> StationCentroidFile;
+        [Parameter(Name = "Station Centroid File", Description = "",
+            Index = 3)]
+        public IFunction<string> ZoneCentroidFile;
+        public override void Invoke(ModellerController context)
+        {
+            context.Run(this, "tmg2.Convert.convert_old_ncs_to_new_ncs", JSONParameterBuilder.BuildParameters(writer =>
+            {
+                writer.WriteNumber("old_ncs_scenario", OldScenarioNumber.Invoke());
+                writer.WriteNumber("new_ncs_scenario", NewScenarioNumber.Invoke());
+                writer.WriteString("station_centroid_file", Path.GetFullPath(StationCentroidFile.Invoke()));
+                writer.WriteString("zone_centroid_file", Path.GetFullPath(ZoneCentroidFile.Invoke()));
+
+            }), LogbookLevel.Standard);
+        }
+    }
+}
