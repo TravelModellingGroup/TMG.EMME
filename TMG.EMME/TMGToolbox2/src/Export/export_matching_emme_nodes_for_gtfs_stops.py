323b3ef3b2eb4fcfb680b6b6d762e9d9efa61200
wdiogu <88385256+wdiogu@users.noreply.github.com>
2022-11-14 16:27:49 -0500

    modified the export matching node tool (#158)
    * modified the export matching node tool
    
    * removed dead codes

--- TMG.EMME/TMGToolbox2/src/Export/export_matching_emme_nodes_for_gtfs_stops.py
+++ TMG.EMME/TMGToolbox2/src/Export/export_matching_emme_nodes_for_gtfs_stops.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2017 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+    Copyright 2022 Travel Modelling Group, Department of Civil Engineering, University of Toronto
 
     This file is part of the TMG Toolbox.
 
@@ -26,8 +26,6 @@ from contextlib import contextmanager
 # from contextlib import nested
 from os import path as _path
 from pyproj import Proj
-from osgeo import ogr
-import osgeo.ogr
 
 _m.InstanceType = object
 _m.TupleType = object
@@ -38,23 +36,21 @@ _util = _MODELLER.module("tmg2.utilities.general_utilities")
 _tmgTPB = _MODELLER.module("tmg2.utilities.TMG_tool_page_builder")
 _geo = _MODELLER.module("tmg2.utilities.geometry")
 _spindex = _MODELLER.module("tmg2.utilities.spatial_index")
-networkExportTool = _MODELLER.tool("inro.emme.data.network.export_network_as_shapefile")
-gtfsExportTool = _MODELLER.tool("tmg2.Convert.convert_gtfs_stops_to_shapefile")
 EMME_VERSION = _util.get_emme_version(tuple)
 
 
 class GTFStoEmmeMap(_m.Tool()):
-    version = "0.0.2"
+    version = "2.0.0"
     tool_run_msg = ""
     number_of_tasks = 1
 
     # Tool Parameters
-    FileName = _m.Attribute(str)
-    MappingOutputFile = _m.Attribute(str)
+    file_name = _m.Attribute(str)
+    mapping_output_file = _m.Attribute(str)
 
     def __init__(self):
         # ---Init internal variables
-        self.TRACKER = _util.progress_tracker(self.number_of_tasks)  # init the progress_tracker
+        self._tracker = _util.progress_tracker(self.number_of_tasks)  # init the progress_tracker
 
     def page(self):
 
@@ -70,14 +66,14 @@ class GTFStoEmmeMap(_m.Tool()):
             pb.tool_run_status(self.tool_run_msg_status)
 
         pb.add_select_file(
-            tool_attribute_name="FileName",
+            tool_attribute_name="file_name",
             window_type="file",
             file_filter="*.txt *.shp",
             title="stops.txt file from the GTFS folder or stops file in *.shp format",
         )
 
         pb.add_select_file(
-            tool_attribute_name="MappingOutputFile",
+            tool_attribute_name="mapping_output_file",
             window_type="save_file",
             file_filter="*.csv",
             title="Map file to export",
@@ -85,16 +81,10 @@ class GTFStoEmmeMap(_m.Tool()):
 
         return pb.render()
 
-    def __call__(self, StopFileName, MappingOutputFile):
-        self.FileName = StopFileName
-        self.MappingOutputFile = MappingOutputFile
-        self.scenarioNumber = ScenarioNumber
-
-        self.tool_run_msg = ""
-        self.TRACKER.reset()
+    def __call__(self, parameters):
 
         try:
-            self._Execute()
+            self._execute(parameters)
         except Exception as e:
             self.tool_run_msg = _m.PageBuilder.format_exception(e, _traceback.format_exc())
             raise
@@ -103,10 +93,10 @@ class GTFStoEmmeMap(_m.Tool()):
 
     def run(self):
         self.tool_run_msg = ""
-        self.TRACKER.reset()
-
+        self._tracker.reset()
+        parameters = self._build_page_builder_parameters
         try:
-            self._Execute()
+            self._execute(parameters)
         except Exception as e:
             self.tool_run_msg = _m.PageBuilder.format_exception(e, _traceback.format_exc())
             raise
@@ -114,193 +104,191 @@ class GTFStoEmmeMap(_m.Tool()):
         self.tool_run_msg = _m.PageBuilder.format_info("Done")
 
     def run_xtmf(self, parameters):
-        self.FileName = parameters["input_stop_file"]
-        self.MappingOutputFile = parameters["output_mapping_file"]
         try:
-            self._Execute()
+            self._execute(parameters)
         except Exception as e:
             raise Exception(_traceback.format_exc())
 
-    def _Execute(self):
-        with _m.logbook_trace(
-            name="{classname} v{version}".format(classname=(self.__class__.__name__), version=self.version),
-            attributes=self._GetAtts(),
-        ):
+    def _execute(self, parameters):
+        with _m.logbook_trace(name="{classname} v{version}".format(classname=(self.__class__.__name__), version=self.version), attributes=self._get_atts()):
             # def file type
-            if self.FileName[-3:].lower() == "txt":
-                stops = self._LoadStopsTxt()
-            elif self.FileName[-3:].lower() == "shp":
-                stops = self._LoadStopsShp()
+            if parameters["input_stop_file"][-3:].lower() == "txt":
+                stops = self._load_stops_txt(parameters["input_stop_file"])
+            elif parameters["input_stop_file"][-3:].lower() == "shp":
+                stops = self._load_stops_shp(parameters["input_stop_file"])
             else:
                 raise Exception("Not a correct format")
             # need to convert stops from lat lon to UTM
-            convertedStops = self._ConvertStops(stops)
+            converted_stops = self._convert_stops(stops)
             # load nodes from network
-            allnodes = _MODELLER.scenario.get_network().regular_nodes()
+            all_nodes = _MODELLER.scenario.get_network().regular_nodes()
             # create node dictionary like converted stops?
             nodes = {}
-            for n in allnodes:
+            for n in all_nodes:
                 nodes[int(n.id)] = (float(n.x), float(n.y))
             # find extents
-            extents = self._FindExtents(convertedStops, nodes)
+            extents = self._find_extents(converted_stops, nodes)
             # load and find nearest point
-            mapping = self._FindNearest(extents, convertedStops, nodes)
-            self._StoreNearest(mapping)
+            mapping = self._find_nearest(extents, converted_stops, nodes)
+            self._store_nearest(mapping, parameters["output_mapping_file"])
 
-    def _GetAtts(self):
+    def _get_atts(self):
         atts = {"Version": self.version, "self": self.__MODELLER_NAMESPACE__}
 
         return atts
 
-    def _LoadStopsTxt(self):
+    def _load_stops_txt(self, file_name):
         stops = {}
-        with open(self.FileName) as reader:
+        with open(file_name) as reader:
             # stop_lat,zone_id,stop_lon,stop_id,stop_desc,stop_name,location_type
             header = reader.readline().strip().split(",")
-            latCol = header.index("stop_lat")
-            lonCol = header.index("stop_lon")
-            idCol = header.index("stop_id")
-            nameCol = header.index("stop_name")
-            descCol = header.index("stop_desc")
+            lat_col = header.index("stop_lat")
+            lon_col = header.index("stop_lon")
+            id_col = header.index("stop_id")
+            name_col = header.index("stop_name")
+            desc_col = header.index("stop_desc")
 
             for line in reader.readlines():
                 cells = line.strip().split(",")
-                id = cells[idCol]
-                stop = GtfsStop(id, cells[lonCol], cells[latCol], cells[nameCol], cells[descCol])
-                stops[id] = [float(cells[lonCol]), float(cells[latCol])]
+                id = cells[id_col]
+                stop = gtfs_stop(id, cells[lon_col], cells[lat_col], cells[name_col], cells[desc_col])
+                stops[id] = [float(cells[lon_col]), float(cells[lat_col])]
         return stops
 
-    def _LoadStopsShp(self):
+    def _load_stops_shp(self, file_name):
         stops = {}
-        with _geo.Shapely2ESRI(self.FileName, "r") as reader:
+        with _geo.Shapely2ESRI(file_name, "r") as reader:
             for point in reader.readThrough():
                 id = str(point.properties["stop_id"])
                 lat = float(point.properties["stop_lat"])
                 lon = float(point.properties["stop_lon"])
-
                 stops[id] = [lon, lat]
-
         return stops
 
-    def _ConvertStops(self, stops):
-        convertedStops = {}
+    def _convert_stops(self, stops):
+        converted_stops = {}
         # find what zone system the file is using
-        fullzonestring = _m.Modeller().desktop.project.spatial_reference_file
+        full_zone_string = _m.Modeller().desktop.project.spatial_reference_file
         if EMME_VERSION >= (4, 3, 0):
-            with open(fullzonestring, "r") as zoneFile:
-                zoneString = zoneFile.read()
-                hemisphere = zoneString[28:29]
-                prjzone = int(zoneString[26:28])
+            with open(full_zone_string, "r") as zone_file:
+                zone_string = zone_file.read()
+                hemisphere = zone_string[28:29]
+                project_zone = int(zone_string[26:28])
         else:
-            hemisphere = fullzonestring[-5:-4]
-            prjzone = int(fullzonestring[-7:-5])
+            hemisphere = full_zone_string[-5:-4]
+            project_zone = int(full_zone_string[-7:-5])
         # put try and exception statements here?
         if hemisphere.lower() == "s":
-            p = Proj("+proj=utm +ellps=WGS84 +zone=%d +south" % prjzone)
+            p = Proj("+proj=utm +ellps=WGS84 +zone=%d +south" % project_zone)
         else:
-            p = Proj("+proj=utm +ellps=WGS84 +zone=%d" % prjzone)
-        stoplons = ()
-        stoplats = ()
+            p = Proj("+proj=utm +ellps=WGS84 +zone=%d" % project_zone)
         for stop in stops.keys():
-            templons = (float(stops[stop][0]),)
-            templats = (float(stops[stop][1]),)
-            x, y = p(templons, templats)
-            convertedStops[stop] = x + y
-            convertedStops[stop] = (
-                float(convertedStops[stop][0]),
-                float(convertedStops[stop][1]),
+            temp_lons = (float(stops[stop][0]),)
+            temp_lats = (float(stops[stop][1]),)
+            x, y = p(temp_lons, temp_lats)
+            converted_stops[stop] = x + y
+            converted_stops[stop] = (
+                float(converted_stops[stop][0]),
+                float(converted_stops[stop][1]),
             )
-        return convertedStops
+        return converted_stops
 
-    def _FindExtents(self, convertedStops, nodes):
+    def _find_extents(self, converted_stops, nodes):
         # find extents
-        maxExtentx = float("-inf")
-        minExtentx = float("inf")
-        maxExtenty = float("-inf")
-        minExtenty = float("inf")
-        for key in convertedStops:
-            if convertedStops[key][0] < minExtentx:
-                minExtentx = float(convertedStops[key][0])
-            if convertedStops[key][0] > maxExtentx:
-                maxExtentx = float(convertedStops[key][0])
-            if convertedStops[key][1] < minExtenty:
-                minExtenty = float(convertedStops[key][1])
-            if convertedStops[key][1] > maxExtenty:
-                maxExtenty = float(convertedStops[key][1])
+        max_extent_x = float("-inf")
+        min_extent_x = float("inf")
+        max_extent_y = float("-inf")
+        min_extent_y = float("inf")
+        for key in converted_stops:
+            if converted_stops[key][0] < min_extent_x:
+                min_extent_x = float(converted_stops[key][0])
+            if converted_stops[key][0] > max_extent_x:
+                max_extent_x = float(converted_stops[key][0])
+            if converted_stops[key][1] < min_extent_y:
+                min_extent_y = float(converted_stops[key][1])
+            if converted_stops[key][1] > max_extent_y:
+                max_extent_y = float(converted_stops[key][1])
         for node in nodes:
-            if nodes[node][0] < minExtentx:
-                minExtentx = float(nodes[node][0])
-            if nodes[node][0] > maxExtentx:
-                maxExtentx = float(nodes[node][0])
-            if nodes[node][1] < minExtenty:
-                minExtenty = float(nodes[node][1])
-            if nodes[node][1] > maxExtenty:
-                maxExtenty = float(nodes[node][1])
-        extents = (minExtentx - 1, minExtenty - 1, maxExtentx + 1, maxExtenty + 1)
+            if nodes[node][0] < min_extent_x:
+                min_extent_x = float(nodes[node][0])
+            if nodes[node][0] > max_extent_x:
+                max_extent_x = float(nodes[node][0])
+            if nodes[node][1] < min_extent_y:
+                min_extent_y = float(nodes[node][1])
+            if nodes[node][1] > max_extent_y:
+                max_extent_y = float(nodes[node][1])
+        extents = (min_extent_x - 1, min_extent_y - 1, max_extent_x + 1, max_extent_y + 1)
         return extents
 
-    def _FindNearest(self, extents, convertedStops, nodes):
+    def _find_nearest(self, extents, converted_stops, nodes):
         map = []
-        spatialIndex = _spindex.GridIndex(extents, 1000, 1000)
+        spatial_index = _spindex.GridIndex(extents, 1000, 1000)
         network = _MODELLER.scenario.get_network()
         for node in network.regular_nodes():
-            spatialIndex.insertPoint(node)
-        for stop in convertedStops:
-            nearestNode = spatialIndex.nearestToPoint(convertedStops[stop][0], convertedStops[stop][1])
-            if nearestNode[0] == "Nothing Found":
+            spatial_index.insertPoint(node)
+        for stop in converted_stops:
+            nearest_node = spatial_index.nearestToPoint(converted_stops[stop][0], converted_stops[stop][1])
+            if nearest_node[0] == "Nothing Found":
                 map.append(
                     [
                         stop,
-                        nearestNode[0],
-                        convertedStops[stop][0],
-                        convertedStops[stop][1],
+                        nearest_node[0],
+                        converted_stops[stop][0],
+                        converted_stops[stop][1],
                         -1,
                         -1,
                     ]
                 )
-            elif nearestNode[0] is None:
+            elif nearest_node[0] is None:
                 map.append(
                     [
                         stop,
-                        nearestNode[0],
-                        convertedStops[stop][0],
-                        convertedStops[stop][1],
+                        nearest_node[0],
+                        converted_stops[stop][0],
+                        converted_stops[stop][1],
                         0,
                         0,
                     ]
                 )
             else:
-                cleanedNumber = int(nearestNode[0])
+                cleaned_number = int(nearest_node[0])
                 map.append(
                     [
                         stop,
-                        cleanedNumber,
-                        convertedStops[stop][0],
-                        convertedStops[stop][1],
-                        nodes[cleanedNumber][0],
-                        nodes[cleanedNumber][1],
+                        cleaned_number,
+                        converted_stops[stop][0],
+                        converted_stops[stop][1],
+                        nodes[cleaned_number][0],
+                        nodes[cleaned_number][1],
                     ]
                 )
         return map
 
-    def _StoreNearest(self, map):
-        with open(self.MappingOutputFile, "w", newline="") as csvfile:
-            mapFile = csv.writer(csvfile, delimiter=",")
+    def _store_nearest(self, map, mapping_output_file):
+        with open(mapping_output_file, "w", newline="") as csv_file:
+            map_file = csv.writer(csv_file, delimiter=",")
             header = ["stopID", "emmeID", "stop x", "stop y", "node x", "node y"]
-            mapFile.writerow(header)
+            map_file.writerow(header)
             for row in map:
-                mapFile.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
+                map_file.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
+
+    def _build_page_builder_parameters(self):
+        parameters = {
+            "file_name": self.file_name,
+            "mapping_output_file": self.mapping_output_file,
+        }
+        return parameters
 
     @_m.method(return_type=_m.TupleType)
     def percent_completed(self):
-        return self.TRACKER.get_progress()
+        return self._tracker.get_progress()
 
     @_m.method(return_type=str)
     def tool_run_msg_status(self):
         return self.tool_run_msg
 
 
-class GtfsStop:
+class gtfs_stop:
     def __init__(self, id, lon, lat, name, description):
         self.id = id
         self.lat = float(lat)
